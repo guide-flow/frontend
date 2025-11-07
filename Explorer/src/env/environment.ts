a749d48c5f7f4cb683a6a7366561256d8540bcfc
Aleksa Kozic <kozicaleksa02@gmail.com>
2025-11-07 01:42:07 +0100

    fix: update API host URLs for environment configuration

--- Explorer/src/env/environment.ts
+++ Explorer/src/env/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  gatewayHost: 'http://20.240.86.21:5230/',
-  toursApiHost: 'http://20.240.86.21:5230/api/tours/',
+  gatewayHost: 'http://soa-app-thesis.swedencentral.cloudapp.azure.com/api/',
+  toursApiHost: 'http://soa-app-thesis.swedencentral.cloudapp.azure.com/api/api/tours/',
 };
