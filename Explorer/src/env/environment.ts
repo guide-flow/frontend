bb00ffc71ecbb0f964f92e5a982de6d7519afd39
Aleksa Kozic <kozicaleksa02@gmail.com>
2025-11-10 00:49:04 +0100

    feat: add GitHub Actions workflow for Azure frontend deployment and update environment API hosts

--- Explorer/src/env/environment.ts
+++ Explorer/src/env/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  gatewayHost: 'http://soa-app-thesis.swedencentral.cloudapp.azure.com/api/',
-  toursApiHost: 'http://soa-app-thesis.swedencentral.cloudapp.azure.com/api/api/tours/',
+  gatewayHost: 'api/',
+  toursApiHost: 'api/tours/',
 };
